5ae40b6a192a9a03d9035dc44c1f4d204b2e952e
LIYonghao21440565 <21440565@life.hkbu.edu.hk>
2022-04-17 11:50:21 +0800

    加载数据，增加功能

--- public/Vue.js
+++ public/Vue.js
@@ -1,86 +1,75 @@
 var app = new Vue({
-    el: '#app',
-    data: {
+  el: '#app',
+  data: function () {
+    return {
+      baseurl: 'http://127.0.0.1:8000',
       isActive: false,
-      selected: -1,
-      selectedlist: {},
-      slist: [],
+
+      // ------------------------------------------------------
+      Admin_selected: -1,
+      Adimin_selectedlist: {},
+      Admin_slist: [],
       searchlist: [],
-      list: [],
+      Admin_list: [],  //Adimin的全部数据
+      // ------------------------------------------------------
+
+
+    }
+  },
+
+  methods: {
+
+    // 点击保存按钮
+    Admin_modify: function (arr) {
+      if (this. Admin_selected> -1) {
+        Vue.set(this.Admin_list, this.Admin_selected, arr);
+        this.Admin_selected = -1;
+      } else {
+        this.Admin_list.push(arr);  //新增的数据存储在Admin_list里面
+      }
+      this.setSlist(this.Admin_list);
+      this.changeOverlay();
     },
-    created() {
+    Admin_add: function () {
+      this.Adimin_selectedlist = {
+        aid: '',
+        aname: '',
+        password: '',
+        addtime: '',
+      };
+      this.isActive = true;
+    },
+
+
+
+
+    // delete list in index location
+    del(index) {
+      this.list.splice(index, 1);
       this.setSlist(this.list);
     },
-    methods: {
-      
-      // 修改数据
-      showOverlay(index) {
-        this.selected = index;
-        this.selectedlist = this.list[index];
-        this.changeOverlay();
-      },
-      // 点击保存按钮
-      modify(arr) {
-        if (this.selected > -1) {
-          Vue.set(this.list, this.selected, arr);
-          this.selected = -1;
-        } else {
-          this.list.push(arr);
-        }
-        this.setSlist(this.list);
-        this.changeOverlay();
-      },
-      add: function () {
-        this.selectedlist = {
-          username: '',
-          email: '',
-          sex: '男',
-          province: '北京市',
-          hobby: []
-        };
-        this.isActive = true;
-      },
-      // delete list in index location
-      del(index) {
-        this.list.splice(index, 1);
-        this.setSlist(this.list);
-      },
-      changeOverlay() {
-        this.selected = -1;
-        this.isActive = !this.isActive;
-      },
-      // 获取需要渲染到页面中的数据
-      setSlist(arr) {
-        this.slist = JSON.parse(JSON.stringify(arr));
-      },
-      // 搜索
-      search(e) {
-        var v = e.target.value,
-          self = this;
-        self.searchlist = [];
-        if (v) {
-          var ss = [];
-          // 过滤需要的数据
-          this.list.forEach(function (item) {
-            if (item.username.indexOf(v) > -1) {
-              if (self.searchlist.indexOf(item.username) == -1) {
-                self.searchlist.push(item.username);
-              }
-              ss.push(item);
-            } else if (item.email.indexOf(v) > -1) {
-              if (self.searchlist.indexOf(item.email) == -1) {
-                self.searchlist.push(item.email);
-              }
-              ss.push(item);
-            }
-          });
-          this.setSlist(ss); // 将过滤后的数据给了slist
-        } else {
-          // 没有搜索内容，则展示全部数据
-          this.setSlist(this.list);
-        }
-      }
+    changeOverlay() {
+      this.selected = -1;
+      this.isActive = !this.isActive;
     },
-    watch: {}
-  })
-  
+    // 获取需要渲染到页面中的数据
+    setSlist(arr) {
+      this.slist = JSON.parse(JSON.stringify(arr));
+    },
+
+  },
+
+  mounted: function () {
+
+    // 首先把后端第一个表的全部数据传进列表里
+    axios.get(this.baseurl + "/api/**************/").then((res) => {
+      this.Admin_list = res.data;
+    })
+
+  },
+
+  watch: {}
+}
+
+
+)
